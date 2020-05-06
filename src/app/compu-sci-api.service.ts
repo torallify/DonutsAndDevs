f5e802755972fd551a93eea84a5de2a8fc2f255d
Lucero Toral <lucerotoral@quickenloans.com>
2020-05-06 16:30:21 -0400

    updating compu-sci-api and donu-api services

--- src/app/compu-sci-api.service.ts
+++ src/app/compu-sci-api.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FamousPeople, Person } from './interfaces/famousPeople';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompuSciAPIService {
+  apiUrl = 'https://grandcircusco.github.io/demo-apis/computer-science-hall-of-fame.json';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getFamousPeople():Observable<FamousPeople>
+  {
+    return this.http.get<FamousPeople>(`${this.apiUrl}`);
+  }
 }
--- src/app/donut-api.service.ts
+++ src/app/donut-api.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Donuts, Donut } from './interfaces/donuts';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DonutAPIService {
+  apiUrl = 'https://grandcircusco.github.io/demo-apis/donuts.json';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getDonuts():Observable<Donuts>
+  {
+    return this.http.get<Donuts>(`${this.apiUrl}`);
+  }
+
 }
